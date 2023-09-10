435ed29af8fd747af60b01e2e973760b10ac593a
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-09-10 11:54:15 +0530

    Rotate Image

--- Java/leetcode/medium/RotateImage.java
+++ Java/leetcode/medium/RotateImage.java
@@ -12,6 +12,7 @@ public class RotateImage {
 
         int n = matrix.length;
 
+        //TRANSPOSE
         for(int i = 0 ; i < n ; i++){
             for(int j = i+1 ; j < n ; j++){
                 if(i != j){
