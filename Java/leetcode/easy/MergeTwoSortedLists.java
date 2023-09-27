e01e6ff7fdb97a030d58eabb66b4ca092e4a6161
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-09-27 21:34:54 +0530

    Merge two sorted lists

--- Java/leetcode/easy/MergeTwoSortedLists.java
+++ Java/leetcode/easy/MergeTwoSortedLists.java
@@ -26,15 +26,17 @@ public class MergeTwoSortedLists {
             currentNode = currentNode.next;
         }
 
-        if(list1 != null){
-            currentNode.next = list1;
-            list1 = list1.next;
-        }
+        // if(list1 != null){
+        //     currentNode.next = list1;
+        //     list1 = list1.next;
+        // }
 
-        if(list2 != null){
-            currentNode.next = list2;
-            list2 = list2.next;
-        }
+        // if(list2 != null){
+        //     currentNode.next = list2;
+        //     list2 = list2.next;
+        // }
+
+         currentNode.next = (list1 != null) ? list1 : list2;
 
         return tempNode.next;
     }
