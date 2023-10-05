a89c8900f31dfeb33836bfec108c0ff73de27396
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-10-05 11:04:54 +0530

    Rotate image

--- Java/leetcode/medium/RotateImage.java
+++ Java/leetcode/medium/RotateImage.java
@@ -1,7 +1,5 @@
 package Java.leetcode.medium;
 
-import java.util.Arrays;
-
 public class RotateImage {
     public static void main(String[] args) {
         int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}}; 
