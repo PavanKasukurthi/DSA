97347168ab39c5e34b57c59f9698b9cfd68b8aca
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-10-14 16:12:31 +0530

    Length of Longest substring without repeating characters

---
+++ Java/leetcode/medium/LongestSubstringWithoutRepeatingChars.java
@@ -0,0 +1,30 @@
+package Java.leetcode.medium;
+
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingChars {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+     public static int lengthOfLongestSubstring(String s) {
+        int i = 0, j = 0, max = 0;
+        
+        HashSet<Character> set = new HashSet<>();
+
+        while(j < s.length()){
+            if(!set.contains(s.charAt(j))){
+                set.add(s.charAt(j));
+                j++;
+                max = Math.max(max, set.size());
+            }
+            else{
+                set.remove(s.charAt(i));
+                i++;
+            }
+        }
+
+        return max;
+    }
+}
