3af9e66e9b3d2ae228d5bad5b94ea0b90c3dd8f9
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2023-11-22 15:49:43 +0530

    Maximum SubArray Sum

---
+++ Java/leetcode/medium/MaxSubArray.java
@@ -0,0 +1,21 @@
+package Java.leetcode.medium;
+
+public class MaxSubArray {
+    public static void main(String[] args) {
+    //    int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] nums = {5,4,-1,7,8};
+       System.out.println(maxSubArraySum(nums));
+    }
+
+    public static int maxSubArraySum(int[] arr){
+        int maxSum = arr[0];
+        int currSum = maxSum;
+
+        for(int i = 1; i < arr.length ; i++){
+            currSum = Math.max(arr[i], arr[i] + currSum);
+            maxSum = Math.max(currSum, maxSum);
+        }
+
+        return maxSum;
+    }
+}
