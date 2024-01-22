ff462e89157244d074835a44f87c29729baecfda
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2024-01-22 12:46:27 +0530

    House Robber

---
+++ Java/leetcode/medium/HouseRobber.java
@@ -0,0 +1,28 @@
+package Java.leetcode.medium;
+
+public class HouseRobber {
+    public static void main(String[] args) {
+        int[] nums = {2,7,9,3,1};
+        System.out.println(rob(nums));
+    }
+
+     public static int rob(int[] nums) {
+
+        if(nums.length == 0) return 0;
+
+        int[] dp = new int[nums.length + 1];
+
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i = 1 ; i < nums.length ; i++){
+            dp[i+1] = Math.max(dp[i], dp[i-1] + nums[i]);
+        }
+
+       return dp[nums.length];
+    }
+}
+
+
+/* EXPLANATION WITH DP TEMPLATE
+https://www.youtube.com/watch?v=ZwDDLAeeBM0 */
