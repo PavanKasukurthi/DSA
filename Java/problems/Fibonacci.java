0aaaa5c699be1f3a6690e76e154c0c2ff4f1b2f9
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2024-01-26 08:12:18 +0530

    Fibonacci dynamic programming

--- Java/problems/Fibonacci.java
+++ Java/problems/Fibonacci.java
@@ -1,9 +1,11 @@
 package Java.problems;
 
+import java.util.HashMap;
+
 public class Fibonacci {
     
     public static void main(String[] args) {
-        System.out.println(fibonacci(3));
+        System.out.println(dynamicFibonacci(5));
     }
     //Recursive approach
     public static int fibonacci(int n){
@@ -12,4 +14,23 @@ public class Fibonacci {
         else return fibonacci(n-1) + fibonacci(n-2);
     }
 
+    //DYNAMIC PROGRAMMING
+    //MEMOIZATION
+    public static int dynamicFibonacci(int n){
+       return fibonacciHelper(n, new HashMap<>());
+    }
+
+    public static int fibonacciHelper(int n, HashMap<Integer, Integer> memo){
+        if(n == 0 || n == 1){
+            return n;
+        }
+
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+
+        int result = dynamicFibonacci(n-1) + dynamicFibonacci(n-2);
+        memo.put(n, result);
+        return result;
+    }
 }
