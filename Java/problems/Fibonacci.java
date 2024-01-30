1908dff80e10e36bc1974e401432a58e2461d3ec
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2024-01-30 21:45:23 +0530

    Dynamic Programming using array

--- Java/problems/Fibonacci.java
+++ Java/problems/Fibonacci.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Fibonacci {
     
     public static void main(String[] args) {
-        System.out.println(dynamicFibonacci(5));
+        System.out.println(dynamicFibonacci2(4));
     }
     //Recursive approach
     public static int fibonacci(int n){
@@ -15,7 +15,7 @@ public class Fibonacci {
     }
 
     //DYNAMIC PROGRAMMING
-    //MEMOIZATION
+    //MEMOIZATION USING HASHMAP
     public static int dynamicFibonacci(int n){
        return fibonacciHelper(n, new HashMap<>());
     }
@@ -32,5 +32,27 @@ public class Fibonacci {
         int result = dynamicFibonacci(n-1) + dynamicFibonacci(n-2);
         memo.put(n, result);
         return result;
+    }
+
+    //MEMOIZATION USING ARRAY
+    public static int dynamicFibonacci2(int n){
+        int[] memo = new int[n+1];
+
+        for(int i = 0 ; i < memo.length ; i++){
+            memo[i] = -1;
+        }
+
+        return dynamicFibonacci2(n, memo);
+    }
+
+    public static int dynamicFibonacci2(int n, int[] memo){
+
+        if(n == 1 || n == 2) return 1;
+
+        if(memo[n] != -1) return memo[n];
+
+        memo[n] = dynamicFibonacci2(n-1, memo) + dynamicFibonacci2(n-2, memo);
+
+        return memo[n];
     }
 }
