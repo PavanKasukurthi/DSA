866bbd3af2652748a4a4ef640b07e2729f334a38
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2024-02-01 11:26:25 +0530

    Bulbs

---
+++ Java/interviewbit/easy/Bulbs.java
@@ -0,0 +1,33 @@
+/* Given N bulbs, either on (1) or off (0)
+ * Turning on ith bulb causes all remaining bulbs on the right to flip
+ * Find the minimum number of switches to turn on all the bulbs
+ * 
+ * Constraints:
+ * 1 <= N <= 1e5
+ * A[i] = {0,1}
+ * 
+ * https://www.interviewbit.com/problems/interview-questions/
+*/
+
+package Java.interviewbit.easy;
+
+public class Bulbs {
+    public static void main(String[] args) {
+        int[] bulbs = {0,1,0,1};
+        System.out.println(bulbSwitch(bulbs));
+    }
+
+    public static int bulbSwitch(int[] bulbs){
+        int cost = 0;
+
+        for(int bulb : bulbs){
+            boolean isFlippedEvenTimes = cost%2 == 0;
+
+            boolean isSwitchRequired = (bulb == 0 && isFlippedEvenTimes || bulb == 1 && !isFlippedEvenTimes);
+
+            if(isSwitchRequired) cost++;
+        }
+
+        return cost;
+    }
+}
