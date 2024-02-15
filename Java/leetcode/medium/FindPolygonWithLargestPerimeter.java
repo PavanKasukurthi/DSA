7386199c889dcc2a32c0d719803f1605fdeb1060
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2024-02-15 11:23:30 +0530

    Find Polygon With Largest Perimeter

---
+++ Java/leetcode/medium/FindPolygonWithLargestPerimeter.java
@@ -0,0 +1,22 @@
+package Java.leetcode.medium;
+
+import java.util.Arrays;
+
+public class FindPolygonWithLargestPerimeter {
+     public long largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+
+        long sum = 0;
+        long ans = -1;
+
+        for(int i = 0 ; i < nums.length; i++){
+            if(sum > nums[i]){
+                ans = nums[i] + sum;
+            }
+                sum += nums[i];
+        }
+
+        return ans;
+
+    }
+}
