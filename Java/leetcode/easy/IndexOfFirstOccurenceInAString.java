4d5532093ce022044bf0bb494abdf5276949dbf7
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2024-07-30 15:55:58 +0530

    using for loop

--- Java/leetcode/easy/IndexOfFirstOccurenceInAString.java
+++ Java/leetcode/easy/IndexOfFirstOccurenceInAString.java
@@ -5,6 +5,7 @@ public class IndexOfFirstOccurenceInAString {
         System.out.println(strStr("sadbutsad", "sad"));
     }
 
+    //USING WHILE LOOP
     public static int strStr(String hayStack, String needle){
         int left = 0;
         int right = needle.length();
@@ -15,6 +16,18 @@ public class IndexOfFirstOccurenceInAString {
             }
             left+=1;
             right+=1;
+        }
+        return -1;
+    }
+
+    //USING FOR LOOP
+    public int strStr2(String haystack, String needle) {
+        int i = 0;
+        for(int j = needle.length() ; j <= haystack.length() ; j++){
+            if(haystack.substring(i,j).equals(needle)){
+                return i;
+            }
+            i++;
         }
         return -1;
     }
