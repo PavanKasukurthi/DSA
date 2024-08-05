72bcf04a172de84ac4a79ba350e551d5e62f7f12
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2024-08-05 13:29:07 +0530

    Kth Distinct String In An Array

---
+++ Java/leetcode/easy/KthDistinctStringInAnArray.java
@@ -0,0 +1,29 @@
+package Java.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KthDistinctStringInAnArray {
+    public static void main(String[] args) {
+        String[] arr = {"d","b","c","b","c","a"};
+        int k = 2;
+        System.out.println(kthDistinct(arr, k));
+    }
+
+    public static String kthDistinct(String[] arr, int k) {
+        HashMap<String, Integer> frequencyCounter = new HashMap<>();
+
+        for(String s : arr){
+            frequencyCounter.put(s, frequencyCounter.getOrDefault(s, 0) + 1);
+        }
+
+        for(String s : arr){
+            if(frequencyCounter.get(s) == 1){
+                --k;
+                if(k == 0) return s;
+            }
+        }
+
+        return "";
+    }
+}
