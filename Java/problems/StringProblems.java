2a857c4f0afeecbe2b3eead9636d9e4a6767f258
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2025-03-16 23:28:35 +0530

    Added more String questions

--- Java/problems/StringProblems.java
+++ Java/problems/StringProblems.java
@@ -103,4 +103,22 @@ public class StringProblems {
         }
         return true;
     }
+
+    /* IsSubSequence */
+    public static boolean isSubsequence(String s, String t){
+        if(s.length() == 0 || s.isEmpty()) return true;
+
+        int i = 0, j = 0;
+
+        while(i < s.length() && j < t.length()){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+
+        if(i == s.length()) return true;
+
+        return false;
+    }  
 }
