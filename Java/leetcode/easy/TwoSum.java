563a3072bd2473488372c3b00d5e9245400a9ccf
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2025-04-10 21:17:37 +0530

    Two Sum

--- Java/leetcode/easy/TwoSum.java
+++ Java/leetcode/easy/TwoSum.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int[] arr = {3,2,4};
-        System.out.println(Arrays.toString(twoSum(arr,6)));
+        int[] arr = { 3, 2, 4 };
+        System.out.println(Arrays.toString(twoSum(arr, 6)));
     }
-    
-    //BRUTE FORCE
-    public static int[] twoSumBrute(int[] nums, int target){
+
+    // BRUTE FORCE
+    public static int[] twoSumBrute(int[] nums, int target) {
         int[] res = new int[2];
-         for(int i = 0 ; i < nums.length-1; i++){
-            for(int j = i+1 ; j < nums.length; j++){
-                if(nums[i] + nums[j] == target){
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
                     res[0] = i;
                     res[1] = j;
                     break;
@@ -23,17 +23,17 @@ public class TwoSum {
         return res;
     }
 
-    //TWO POINTER 
-    public static int[] twoSum(int[] nums, int target){
+    // TWO POINTER
+    public static int[] twoSum(int[] nums, int target) {
         int[] sortedNums = Arrays.copyOf(nums, nums.length);
         Arrays.sort(sortedNums);
-        
+
         int left = 0;
         int right = nums.length - 1;
-        
+
         while (left < right) {
             int sum = sortedNums[left] + sortedNums[right];
-            
+
             if (sum == target) {
                 int firstIndex = -1;
                 int secondIndex = -1;
@@ -44,14 +44,14 @@ public class TwoSum {
                         secondIndex = i;
                     }
                 }
-                return new int[] {firstIndex, secondIndex};
+                return new int[] { firstIndex, secondIndex };
             } else if (sum < target) {
                 left++;
             } else {
                 right--;
             }
         }
-        
-        return new int[] {-1, -1}; 
+
+        return new int[] { -1, -1 };
     }
 }
