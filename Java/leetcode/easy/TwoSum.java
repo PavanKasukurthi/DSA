0e9adbdfe0954d9f53da0d27a9866bed7411ee9e
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2025-05-11 22:03:28 +0530

    TwoSum using HashMap

--- Java/leetcode/easy/TwoSum.java
+++ Java/leetcode/easy/TwoSum.java
@@ -1,11 +1,12 @@
 package Java.leetcode.easy;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
     public static void main(String[] args) {
         int[] arr = { 3, 2, 4 };
-        System.out.println(Arrays.toString(twoSum(arr, 6)));
+        System.out.println(Arrays.toString(twoSumUsingHashMap(arr, 11)));
     }
 
     // BRUTE FORCE
@@ -50,6 +51,24 @@ public class TwoSum {
             } else {
                 right--;
             }
+        }
+
+        return new int[] { -1, -1 };
+    }
+
+    // USING HASHMAP
+    public static int[] twoSumUsingHashMap(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            map.put(nums[i], i);
         }
 
         return new int[] { -1, -1 };
