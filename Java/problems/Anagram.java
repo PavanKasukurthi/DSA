4dec6bf54046e19e74468c7c02057d8e4f3a106e
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2025-09-22 21:51:48 +0530

    Anagram

--- Java/problems/Anagram.java
+++ Java/problems/Anagram.java
@@ -3,13 +3,14 @@ package Java.problems;
 import java.util.HashMap;
 
 public class Anagram {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(anagram("cinema", "icemaa"));
     }
 
-    public static boolean anagram(String str1, String str2){
+    public static boolean anagram(String str1, String str2) {
 
-        if(str1.length() != str2.length()) return false;
+        if (str1.length() != str2.length())
+            return false;
 
         char[] arr1 = str1.toLowerCase().toCharArray();
         char[] arr2 = str2.toLowerCase().toCharArray();
@@ -17,15 +18,16 @@ public class Anagram {
         HashMap<Character, Integer> frequencyCounter1 = new HashMap<>();
         HashMap<Character, Integer> frequencyCounter2 = new HashMap<>();
 
-        for(Character ch : arr1){
+        for (Character ch : arr1) {
             frequencyCounter1.merge(ch, 1, Integer::sum);
         }
 
-        for(Character ch : arr2){
+        for (Character ch : arr2) {
             frequencyCounter2.merge(ch, 1, Integer::sum);
         }
 
-        if(!(frequencyCounter1.equals(frequencyCounter2))) return false;
+        if (!(frequencyCounter1.equals(frequencyCounter2)))
+            return false;
 
         return true;
     }
