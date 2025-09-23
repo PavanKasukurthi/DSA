38bdff4efdec839876855d6a69242add84fe8e2c
PavanKasukurthi <pavankasukurthi2000@gmail.com>
2025-09-23 22:26:31 +0530

    Merge Strings Alternatively

--- Java/leetcode/easy/MergeStringsAlternately.java
+++ Java/leetcode/easy/MergeStringsAlternately.java
@@ -4,20 +4,22 @@ public class MergeStringsAlternately {
     public static void main(String[] args) {
         System.out.println(mergeStringsAlternately("ab", "pqrs"));
     }
-    
-    public static String mergeStringsAlternately(String a, String b){
+
+    public static String mergeStringsAlternately(String word1, String word2) {
         StringBuilder result = new StringBuilder();
         int i = 0;
         int j = 0;
 
-        while(i < word1.length() && j < word2.length()){
+        while (i < word1.length() && j < word2.length()) {
             result.append(word1.charAt(i++)).append(word2.charAt(j++));
         }
 
-        if(i < word1.length()) result.append(word1.substring(i,word1.length()));
-        
-        if(j < word2.length()) result.append(word2.substring(j, word2.length()));
-        
+        if (i < word1.length())
+            result.append(word1.substring(i, word1.length()));
+
+        if (j < word2.length())
+            result.append(word2.substring(j, word2.length()));
+
         return result.toString();
     }
 }
